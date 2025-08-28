8d8a3677c3031504ec9451215ad9a522096bda46
Royalross <royaltross@gmail.com>
2025-08-28 17:36:27 -0400

    Initial commit

---
+++ HealthApp-Backend/src/main/java/com/osu/HealthApp/HealthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.osu.HealthApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAppBackendApplication.class, args);
+	}
+
+}
