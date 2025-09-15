069b49684c03f22a52bbe0be6caebc5397e32097
Arun Ghimire <dev.arunghimire@gmail.com>
2025-09-15 03:13:41 -0400

    page skeleton

---
+++ healthapp-frontend/app/doctor_dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function DoctorDashboard() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
+        Doctor Dashboard
+      </h1>
+    </div>
+  );
+}
