f625d807a0c17a65cc7c2b341dfc3b9a117c8c28
schmidt1276 <schmidt.1276@buckeyemail.osu.edu>
2025-10-02 21:08:10 -0400

    Allow admin to read a list of all users and associated roles

--- HealthApp-Backend/src/main/java/com/osu/HealthApp/controller/AdminController.java
+++ HealthApp-Backend/src/main/java/com/osu/HealthApp/controller/AdminController.java
@@ -1,16 +1,27 @@
 package com.osu.HealthApp.controller;
 
+import com.osu.HealthApp.models.User;
+import com.osu.HealthApp.models.Role;
+import com.osu.HealthApp.repo.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
+	
+	private final UserRepository users;
+	
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/panel")
-    public Map<String, Object> panel() {
-        return Map.of("section", "admin", "status", "ok");
+    @GetMapping("/getusers")
+    public Map<String, Set<Role>> getUsers() {
+        return users.findAll().stream().collect(Collectors.toMap(User::getEmail, User::getRoles));
     }
 }
