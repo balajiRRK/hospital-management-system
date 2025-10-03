c338c2a0e39d073a099c3f6fef9900899050b158
schmidt1276 <schmidt.1276@buckeyemail.osu.edu>
2025-10-02 22:57:44 -0400

    Add backend admin calls to add and remove roles

--- HealthApp-Backend/src/main/java/com/osu/HealthApp/controller/AdminController.java
+++ HealthApp-Backend/src/main/java/com/osu/HealthApp/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.osu.HealthApp.controller;
 
 import com.osu.HealthApp.models.User;
+import com.osu.HealthApp.models.Context;
 import com.osu.HealthApp.models.Role;
 import com.osu.HealthApp.dtos.SimpleUserRequest;
+import com.osu.HealthApp.dtos.UserRoleRequest;
+import com.osu.HealthApp.dtos.UserRoleResponse;
 import com.osu.HealthApp.repo.UserRepository;
 import com.osu.HealthApp.service.UserService;
 import com.osu.HealthApp.service.AuthService;
@@ -10,12 +13,15 @@ import com.osu.HealthApp.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 @RestController
@@ -46,5 +52,39 @@ public class AdminController {
     public ResponseEntity<Void> reactivate(@Valid @RequestBody SimpleUserRequest req) {
         userService.enableAccount(req.email().trim().toLowerCase());
 		return ResponseEntity.ok().build();
+    }
+	
+	@PreAuthorize("hasRole('ADMIN')")
+    @PostMapping(value = "/addroles", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserRoleResponse> addRoles(@Valid @RequestBody UserRoleRequest req) {
+		String email = req.email().trim().toLowerCase();
+		Set<String> roles = req.roles();
+		Set<Role> enumRoles = new HashSet<Role>();
+		for (String role : roles) {
+			try {
+				enumRoles.add(Role.valueOf(role));
+			} catch (IllegalArgumentException e) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid role: " + role );
+			}
+		}
+		
+        return ResponseEntity.ok(new UserRoleResponse(email, userService.addRoles(req.email().trim().toLowerCase(), enumRoles)));
+    }
+	
+	@PreAuthorize("hasRole('ADMIN')")
+    @PostMapping(value = "/removeroles", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<UserRoleResponse> removeRoles(@Valid @RequestBody UserRoleRequest req) {
+		String email = req.email().trim().toLowerCase();
+		Set<String> roles = req.roles();
+		Set<Role> enumRoles = new HashSet<Role>();
+		for (String role : roles) {
+			try {
+				enumRoles.add(Role.valueOf(role));
+			} catch (IllegalArgumentException e) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid role: " + role );
+			}
+		}
+		
+		return ResponseEntity.ok(new UserRoleResponse(email, userService.removeRoles(email, enumRoles)));
     }
 }
--- HealthApp-Backend/src/main/java/com/osu/HealthApp/service/UserService.java
+++ HealthApp-Backend/src/main/java/com/osu/HealthApp/service/UserService.java
@@ -1,12 +1,18 @@
 package com.osu.HealthApp.service;
 
 import com.osu.HealthApp.models.User;
+import com.osu.HealthApp.models.Role;
 import com.osu.HealthApp.repo.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,4 +35,27 @@ public class UserService {
 		user.setEnabled(true);
 		users.save(user);
     }
+	
+	@Transactional
+	public Set<Role> addRoles(String email, Set<Role> roles) {
+		User user = users.findByEmail(email)
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No such user"));
+		Set<Role> newRoles = Stream.concat(user.getRoles().stream(), roles.stream()).collect(Collectors.toSet());
+		user.setRoles(newRoles);
+		users.save(user);
+		return newRoles;
+	}
+	
+	@Transactional
+	public Set<Role> removeRoles(String email, Set<Role> roles) {
+		if (roles.contains(Role.PATIENT)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot remove patient role");
+		}
+		User user = users.findByEmail(email)
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "No such user"));
+		Set<Role> newRoles = user.getRoles().stream().filter(role -> !roles.contains(role)).collect(Collectors.toSet());
+		user.setRoles(newRoles);
+		users.save(user);
+		return newRoles;
+	}
 }
